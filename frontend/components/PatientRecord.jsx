ac3ac16d934d5a2cf615a9b84d96abaaac231f61
A E <aelmollaz@gmail.com>
2022-06-10 00:25:18 -0400

    Display patient data on homepage for testing api

--- frontend/components/PatientRecord.jsx
+++ frontend/components/PatientRecord.jsx
@@ -1,8 +1,11 @@
 
-const PatientRecord = () => {
+const PatientRecord = ({patient}) => {
   return (
     <div>
-      
+      <h1>Name: {patient.First_name}</h1>
+      <h1>Surname: {patient.Last_name}</h1>
+      <h1>Age: {patient.Age}</h1>
+      <h1>Blood Type: {patient.Blood_type}</h1>
     </div>
   )
 }
