186147635167241666e14d90f12d08b1a2d26752
A E <aelmollaz@gmail.com>
2022-06-12 14:27:12 -0400

    Change details card close button

--- frontend/components/PatientRecord.jsx
+++ frontend/components/PatientRecord.jsx
@@ -1,16 +1,37 @@
-const PatientRecord = ({ patient, setShowDetails, patientInitials}) => {
+const PatientRecord = ({ patient, setShowDetails, patientInitials }) => {
   console.log(545, patient);
-  const {First_name, Last_name, Age, Address, Allergies, Medication, Blood_type, Gender, Ailment, Emergency_contact_name, Emergency_contact_number, Last_visit} = patient
+  const {
+    First_name,
+    Last_name,
+    Age,
+    Address,
+    Allergies,
+    Medication,
+    Blood_type,
+    Gender,
+    Ailment,
+    Emergency_contact_name,
+    Emergency_contact_number,
+    Last_visit,
+  } = patient;
   return (
-    <div className=" absolute h-full flex justify-center items-center w-full top-0 backdrop-blur-md flex-col">
-      <button
-        className="w-20 rounded-md text-white bg-blue-600 hover:bg-white hover:text-blue-600 border-2 hover:border-blue-600 hover:border-2"
-        onClick={() => setShowDetails(prev => !prev)}
-      >
-        Close
-      </button>
-      <div className=" relative py-3 px-6 bg-white font-Roboto font-medium shadow-2xl">
-        <h1 className=" text-center text-3xl font-extrabold mb-5">{patientInitials}</h1>
+    <div className="absolute h-full flex justify-center items-center w-full top-0 backdrop-blur-md flex-col">
+      <div className="rounded-xl relative py-3 px-6 bg-white font-Roboto font-medium shadow-2xl">
+        {/* <button
+          className="w-20 absolute top-0 right-0 rounded-md text-white bg-blue-600 hover:bg-white hover:text-blue-600 border-2 hover:border-blue-600 hover:border-2"
+          onClick={() => setShowDetails((prev) => !prev)}
+        >
+          Close
+        </button> */}
+        <span
+          className="material-icons absolute text-4xl top-0 right-0 text-blue-600  hover:text-blue-700 cursor-pointer"
+          onClick={() => setShowDetails((prev) => !prev)}
+        >
+          cancel
+        </span>
+        <h1 className=" text-center text-3xl font-extrabold mb-5">
+          {patientInitials}
+        </h1>
         <h3 className=" mb-1">Name: {First_name}</h3>
         <h3 className=" mb-1">Surname: {Last_name}</h3>
         <p className=" mb-1">Age: {Age}</p>
@@ -20,7 +41,9 @@ const PatientRecord = ({ patient, setShowDetails, patientInitials}) => {
         <p className=" mb-1">Medication: {Medication}</p>
         <p className=" mb-1">Last visit: {Last_visit}</p>
         <p className=" mb-1">Emergency contact: {Emergency_contact_name}</p>
-        <p className=" mb-1">Emergency contact number: {Emergency_contact_number}</p>
+        <p className=" mb-1">
+          Emergency contact number: {Emergency_contact_number}
+        </p>
         <p className=" mb-1">Address: {Address}</p>
       </div>
     </div>
